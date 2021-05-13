d3b590585d63984061a18061296258cac37734e3
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-12 21:20:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,19 @@ app.get('/', function (request, response) {
 
 app.get('/home', function (request, response) {
     console.log(user_info)
-    response.sendFile(path.join(__dirname, '/public', 'home.html'));
+
+    var holdings = user_info[1].holdings
+    var amount = user_info[1].amount
+    var cryprolist = []
+
+    for (var i = 0; i < holdings.length; i++) {
+        cryprolist.push({ 'holdings': holdings[i], 'amount': amount[i] })
+    }
+
+    var context = []
+    context.cryprolist = cryprolist
+
+    response.sendFile(path.join(__dirname, '/public', 'home.html'), context);
 
 });
 
